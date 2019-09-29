dd005a20cc7fd7c12e4d469adc3d4a943d9c01ff
danny <98850+f96q@users.noreply.github.com>
2019-09-29 15:11:49 +0900

    Add sandbox case

--- javascripts/App.tsx
+++ javascripts/App.tsx
@@ -1,3 +1,60 @@
+/*
 import React from 'react'
+import { DeepReadonly } from 'utility-types'
+
+
+type Foo = DeepReadonly<{
+  first: {
+    second: {
+      name: string
+    }
+  }
+}>
+
+const foo: Foo = {
+  first: {
+    second: {
+      name: 'name'
+    }
+  }
+}
+
+foo.first = { second: 'name2' }
+//foo.first.second.name = 'name2'
 
 export const App: React.FC = () => (<div>app</div>)
+*/
+
+
+import React from 'react'
+import { rgba } from 'polished'
+import styled, { ThemeProvider } from 'styled-components'
+
+type Theme = {
+  colors: {
+    [key: string]: string
+  }
+}
+
+const theme: Theme = {
+  colors: {
+    black: '#000',
+    white: '#fff'
+  }
+}
+
+type Color =
+  | 'black'
+  | 'white'
+
+const color = (color: Color): ((props: { theme: Theme }) => string) => props => props.theme.colors[color]
+
+export const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <Main />
+  </ThemeProvider>
+)
+
+const Main = styled.div`
+  color: ${props => rgba(color('black')(props), 0.3)};
+`
